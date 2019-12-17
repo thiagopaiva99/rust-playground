f52ff03d42d5291656ef129ef012bc261b624bbc
Thiago Galvani <thiagopaiva99@gmail.com>
2019-12-17 21:26:08 -0200

    Creating data types on variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     let x = 45; // -> immutable data
     let mut y = 60; // -> mutable data // DATA
 
+    let a: i64 = 1; // -> data types
+    let b: f32 = 6.5;
+    let c: bool = true;
+
     println!("The value of x is {}", x);
     println!("The value of y is {}", y);
 }
