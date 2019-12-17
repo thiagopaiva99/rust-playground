54a36cc6b5c872dc1a9149e324a34b77d3dd2d40
Thiago Galvani <thiagopaiva99@gmail.com>
2019-12-17 21:24:28 -0200

    Creating project to play with variables

---
+++ variables/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x = 45; // -> immutable data
+    let mut y = 60; // -> mutable data // DATA
+
+    println!("The value of x is {}", x);
+    println!("The value of y is {}", y);
+}
