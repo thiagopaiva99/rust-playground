3822e5a2a63f611e6465e39eb77a4490a2cfa23d
Thiago Galvani <thiagopaiva99@gmail.com>
2019-12-17 21:29:15 -0200

    Playing with conditionals

---
+++ conditionals/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let value: i64 = 58;
+
+    if value < 50 {
+        println!("The value is smaller than 50");
+    } 
+    else if value == 58 {
+        println!("The value is equal to 58");
+    }    
+    else {
+        println!("The value is bigger than 50");
+    }
+}
